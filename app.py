8164c25cbedf5a22c6f84d9faf5d88a96ebb1605
chongyk-cloud <jiaoshui067@gmail.com>
2026-08-23 09:53:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_attraction_photo(attraction_name):
         "Wu Dang Shan": "Wudang Mountains",
         "Kai Feng Fu": "Kaifeng Prefecture",
         "Ning De Yuan Yang Xi": "Ningde"  # <-- Added to fix the cartoon rabbit!
-        "Tian Mu Hu": "Tianmu Lake",
+        "Tian Mu Hu": "Tianmu Lake"
     }
     
     # 2. Advanced Pinyin Translation Dictionaries
