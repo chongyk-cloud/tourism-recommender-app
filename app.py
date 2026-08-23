7406b937d4aeb0992b248a7c684d78cd0cc9a545
chongyk-cloud <jiaoshui067@gmail.com>
2026-08-23 10:01:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,9 @@ def get_attraction_photo(attraction_name):
         "Wu Dang Shan": "Wudang Mountains",
         "Kai Feng Fu": "Kaifeng Prefecture",
         "Ning De Yuan Yang Xi": "Ningde",  # <-- Added to fix the cartoon rabbit!
-        "Tian Mu Hu": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Tianmu_Lake_gate.JPG/600px-Tianmu_Lake_gate.JPG" # <-- Added the exact Wikipedia gate image here!
-    }
+        "Tian Mu Hu": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Tianmu_Lake_gate.JPG/600px-Tianmu_Lake_gate.JPG"
+        "Lao Shan": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Laoshan-mountain-with-rocks.jpg/600px-Laoshan-mountain-with-rocks.jpg"  # <-- Added Mount Lao here!
+}
     
     # 2. Advanced Pinyin Translation Dictionaries
     pinyin_map_2_words = {
