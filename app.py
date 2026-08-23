9763db39c7fe3ccae17444cc4c96ca5e337104c9
chongyk-cloud <jiaoshui067@gmail.com>
2026-08-23 10:09:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def get_attraction_photo(attraction_name):
         "Kai Feng Fu": "Kaifeng Prefecture",
         "Ning De Yuan Yang Xi": "Ningde", 
         "Tian Mu Hu": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Tianmu_Lake_gate.JPG/600px-Tianmu_Lake_gate.JPG" # <-- Added the exact Wikipedia gate image here!
-}
+
     }
 
     
